1103703620784e2be862e315f41cd07f090d8d8c
alanantony24 <s10207327@connect.np.edu.sg>
2023-09-23 18:36:40 +0800

    saves bookData to localStorage

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const showButton = document.getElementById("showDialog");
-const bookDialog = document.getElementById("bookDialog");
-const outputBox = document.querySelector("output");
-const selectEl = bookDialog.querySelector("select");
-const confirmBtn = bookDialog.querySelector("#confirmBtn");
+const showDialogButton = document.getElementById("showDialogButton");
+const addBookDialog = document.getElementById("addBookDialog");
+const confirmBtn = addBookDialog.querySelector("#confirmBtn");
 
 // "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener("click", () => {
-  bookDialog.showModal();
+showDialogButton.addEventListener("click", () => {
+  addBookDialog.showModal();
 });
 
-// "Favorite animal" input sets the value of the submit button
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
-});
+function save() {
+  var author = document.getElementById('author').value;
+  var title = document.getElementById('title').value;
+  var numberOfPages = document.getElementById('pages').value;
+  var read = document.getElementById('read').value;
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-bookDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    bookDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${bookDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
+  const bookData = {
+    author: author,
+    title: title,
+    numberOfPages: numberOfPages,
+    read: read
+  }
+  localStorage.setItem("bookData", JSON.stringify(bookData));
+}
 
 // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
-  bookDialog.close(selectEl.value); // Have to send the select box value here.
+  save();
+  addBookDialog.close(); // Have to send the select box value here.
 });
